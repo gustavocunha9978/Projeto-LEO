bd137821caa8474999c0c0e8a0a6e8e718fedbf6
gustavocunha9978 <gustavocunhasoares@hotmail.com>
2024-03-12 20:01:24 -0300

    lista

--- lista 2/ex6.py
+++ lista 2/ex6.py
@@ -1,1 +1,12 @@
-value_1 = float(input(""))
+num1 = int(input("Put a number: "))
+num2 = int(input("Put a number: "))
+num3 = int(input("Put a number: "))
+
+if num1 < num2:
+    num1, num2 = num2, num1
+if num1 < num3:
+    num1, num3 = num3, num1
+if num2 < num3:
+    num2, num3 = num3, num2
+
+print("Numbers in descending order:", num1, ",", num2, ",", num3)
