e09a2f1bd24285c5d61311995f74c3838c2f7c79
gustavocunha9978 <gustavocunhasoares@hotmail.com>
2024-03-19 11:35:59 -0300

    palindrome

--- AULA/palindromo.py
+++ AULA/palindromo.py
@@ -1,16 +1,16 @@
-def is_palindromo(word):
+def is_palindrome(word):
     word = word.replace(" ", "").lower()
     return word == word[::-1]
 
 def count_words(file_path):
-    palindromo_count = 0
+    palindrome_count = 0
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
         words = content.split()
         for word in words:
-          if is_palindromo(word):
-              palindromo_count += 1
-        return palindromo_count
+          if is_palindrome(word):
+              palindrome_count += 1
+        return palindrome_count
     
 archive = 'book/download_file.txt'
 total_words = count_words(archive)
